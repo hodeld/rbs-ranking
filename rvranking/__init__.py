c937e652fa8ab443d9a875f5c920a153677e7ba5
Damian <42800152+hodeld@users.noreply.github.com>
2020-07-08 14:10:11 +0200

    WIP logs

--- rvranking/__init__.py
+++ rvranking/__init__.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from rvranking.sampling.elwcWrite import write_elwc
-from rvranking.logs import logging_basic, hplogger
+from rvranking.logs import logging_basic, hplogger, logs_to_csv
 from rvranking.model import train_and_eval_fn
 from rvranking.globalVars import (_MODEL_DIR, _FAKE, _LIST_SIZE,
                                   _BATCH_SIZE, _LEARNING_RATE, _DROPOUT_RATE,
@@ -39,7 +39,7 @@ def main_routine(include_comment=True):
     if not IN_COLAB and include_comment:
         comment = input('comment on run: ')
     else:
-        comment = 'in colab'
+        comment = 'in colab or prediction'
     hyparams = {'comment': comment,
                 'fake': _FAKE,
                 'sampling method': _SAMPLING,
@@ -63,7 +63,6 @@ def main_routine(include_comment=True):
 def write_file(hyparams):
     for k, v in hyparams.items():
         hplogger.info(k + ': ' + str(v))
-    hplogger.info('new run -----------------------------------------')
 
 
 def baseline():
@@ -82,19 +81,26 @@ def baseline():
 def predictions():
     ranker = 'd'
 
-    ranker = main_routine()
+    ranker = main_routine(False)
     #ranker.export_saved_model('path', 'serving_inout_receiver_fn')
 
     predicts = make_predictions(ranker)
     p1 = get_next_prediction(predicts)
     print(p1)
+    comment = input('comment on prediction: ')
+    hplogger.info('comment on prediction: ' + comment)
+    hplogger.info('predictions: ' + str(p1))
 
 
 if __name__ == '__main__':
-    logging_basic()
-    #main_routine()
-    baseline()
-    #predictions()  # including train
+    #logging_basic()
+
+    dispatch_fn = {
+        1: main_routine,
+        2: baseline,
+        3: predictions,  # including train
+    }
+    dispatch_fn[4]()
 
 
 
